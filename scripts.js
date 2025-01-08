90704d5014b00635bb01b6e9ba7ba552dbc5872a
vincent <85801240+vincentcomfy@users.noreply.github.com>
2025-01-08 23:27:33 +0800

    added initial screen update logic

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function displayUpdate() {
+    let initialValue = "0";
+    const display = document.querySelector('.calculatorScreen');
+    display.textContent = initialValue;
+}
+
+window.onload = displayUpdate;
+
+function operate(operator, num1, num2) {
+    
+}
